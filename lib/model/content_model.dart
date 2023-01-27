0e82b0ef313979b27027ef8b9a749057e322eaa0
Dongwon Choi <cookie000215@gmail.com>
2023-01-28 02:05:51 +0900

    Add: content schema

---
+++ lib/model/content_model.dart
@@ -0,0 +1,45 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class ContentModel {
+  late String contentId, contentPostId, contentImgUrl, contentText, writerId;
+  late DateTime uploadedAt;
+
+  ContentModel({
+    required this.contentId,
+    required this.contentPostId,
+    required this.contentImgUrl,
+    required this.contentText,
+    required this.uploadedAt,
+    required this.writerId,
+  });
+
+  ContentModel.fromDocumentSnapshot(
+      {required DocumentSnapshot documentSnapshot}) {
+    contentId = documentSnapshot.id;
+    contentPostId = documentSnapshot['contentPostId'];
+    contentImgUrl = documentSnapshot['contentImgUrl'];
+    contentText = documentSnapshot['contentText'];
+    uploadedAt = documentSnapshot['uploadedAt'];
+    writerId = documentSnapshot['writerId'];
+  }
+
+  ContentModel.fromJson(Map<dynamic, dynamic> map) {
+    contentId = map['contentId'];
+    contentPostId = map['contentPostId'];
+    contentImgUrl = map['contentImgUrl'];
+    contentText = map['contentText'];
+    uploadedAt = map['uploadedAt'];
+    writerId = map['writerId'];
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'contentId': contentId,
+      'contentPostId': contentPostId,
+      'contentImgUrl': contentImgUrl,
+      'contentText': contentText,
+      'uploadedAt': uploadedAt,
+      'writerId': writerId,
+    };
+  }
+}
