d78d27770be4eea63f73df36ad0801957465e9cf
Dongwon Choi <cookie000215@gmail.com>
2023-01-29 05:11:38 +0900

    Fix: filrestorage api

--- lib/core/service/firestorage_apis.dart
+++ lib/core/service/firestorage_apis.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:file/file.dart';
+import 'dart:math';
 
 final storageRef = FirebaseStorage.instance.ref();
 Reference imagesRef = storageRef.child("image");
@@ -7,34 +8,45 @@ Reference videoRef = storageRef.child("vedio");
 Reference postRef = imagesRef.child("post");
 Reference contentRef = imagesRef.child("ref");
 
+const _chars = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890';
+Random _rnd = Random();
+String getRandomString(int length) => String.fromCharCodes(Iterable.generate(
+    length, (_) => _chars.codeUnitAt(_rnd.nextInt(_chars.length))));
+
 class FireStorage {
-  Future<String> uploadPostImg(File file, String pid) async {
+  Future<String> uploadPostImg(File file) async {
     try {
-      await postRef.child(pid).putFile(file);
+      String filename = getRandomString(15);
+      Reference postImgRef = postRef.child(filename);
+      await postImgRef.putFile(file);
 
-      return await postRef.getDownloadURL();
+      return await postImgRef.getDownloadURL();
     } on FirebaseException catch (e) {
       print("error: $e");
       return "";
     }
   }
 
-  Future<String> uploadContentImg(File file, String cid) async {
+  Future<String> uploadContentImg(File file) async {
     try {
-      await contentRef.child(cid).putFile(file);
+      String filename = getRandomString(15);
+      Reference contentImgRef = contentRef.child(filename);
+      await contentImgRef.putFile(file);
 
-      return await contentRef.getDownloadURL();
+      return await contentImgRef.getDownloadURL();
     } on FirebaseException catch (e) {
       print("error: $e");
       return "";
     }
   }
 
-  Future<String> uploadVideo(File file, String pid) async {
+  Future<String> uploadVideo(File file) async {
     try {
-      await videoRef.child(pid).putFile(file);
+      String filename = getRandomString(15);
+      Reference postVideoRef = contentRef.child(filename);
+      await postVideoRef.putFile(file);
 
-      return await videoRef.getDownloadURL();
+      return await postVideoRef.getDownloadURL();
     } on FirebaseException catch (e) {
       print("error: $e");
       return "";
