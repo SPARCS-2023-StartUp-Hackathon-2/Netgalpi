4803ee7f337b6564fc313ed930811e1324adfb9d
asjay18 <sejin1314@korea.ac.kr>
2023-01-28 20:35:21 +0900

    Chore: commit before merge

--- lib/view/components/custom_appbar.dart
+++ lib/view/components/custom_appbar.dart
@@ -10,6 +10,10 @@ class CustomAppBar extends StatelessWidget {
       backgroundColor: const Color.fromRGBO(255, 255, 255, 0.5),
       elevation: 0.0,
       iconTheme: const IconThemeData(color: gray600),
+      leading: Text(
+        'Netgalfi',
+        style: TextStyle(fontSize: 28, letterSpacing: 0.1, color: gray700),
+      ),
     );
   }
 }
