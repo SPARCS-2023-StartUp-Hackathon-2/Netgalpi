ac653736167c9dd95bc38cdfce841f0ac8fed61c
star <snaoyam@gmail.com>
2023-01-29 05:08:52 +0900

    add add-button

---
+++ lib/view/add_content_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class AddContentPage extends StatefulWidget {
+  const AddContentPage({super.key});
+
+  @override
+  State<AddContentPage> createState() => _AddContentPageState();
+}
+
+class _AddContentPageState extends State<AddContentPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      
+    );
+  }
+}
--- lib/view/post_view.dart
+++ lib/view/post_view.dart
@@ -46,6 +46,37 @@ class _PostViewState extends State<PostView> {
         ),
       ),
       extendBodyBehindAppBar: true,
+      floatingActionButton: Container(
+        decoration: const BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.all(
+              Radius.circular(28.0)
+          ),
+          boxShadow: [
+            BoxShadow(blurRadius: 4.0, color: Colors.black26),
+          ],
+        ),
+        width: 104,
+        height: 56,
+        child: Padding(
+          padding: EdgeInsets.all(4),
+          child: TextButton.icon(
+            style: TextButton.styleFrom(
+              foregroundColor: Colors.pink,
+            ),
+            onPressed: () { 
+            
+            }, 
+            icon: Icon(Icons.add, size: 20,), 
+            label: Text("추가하기", 
+            style: TextStyle(
+              fontWeight: FontWeight.bold, 
+              fontSize: 13),
+            )
+          ),
+        )
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(30),
